d8410ba2497cff494e5b5c89423fb2f42156928a
May Thu Pike Soe <maythupikesoe@gmail.com>
2023-04-30 16:19:43 -0600

    weather app

---
+++ src/index.js
@@ -0,0 +1,115 @@
+function dateFormat(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(time) {
+  let date = new Date(time * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+function showForecast(response) {
+  console.log(response.data.daily);
+  let days = response.data.daily;
+  let forecast = document.querySelector("#weather-forecast");
+  let forecastHTML = ` <div class="row">`;
+
+  days.forEach(function (day, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+              <div class="col ">
+                <div class="day" style=" margin-left:10px">${formatDay(
+                  day.dt
+                )}</div>
+
+                <img
+                  src="http://openweathermap.org/img/wn/${
+                    day.weather[0].icon
+                  }@2x.png"
+                  style="width: 40px; height: 40px ;"
+                  alt=""
+                />
+
+                <div class="forecast-degree">
+                  <span class="temperature-max">${Math.round(
+                    day.temp.max
+                  )}</span>°
+                  <span class="temperature-min">${Math.round(
+                    day.temp.min
+                  )}</span>°
+                </div>
+              </div>
+             
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecast.innerHTML = forecastHTML;
+}
+function getForecast(coordinate) {
+  let apiKey = "a710bd8bd76400c9658ef649d9e81728";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinate.lat}&lon=${coordinate.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
+}
+
+function displayWeather(response) {
+  console.log(response.data);
+  let temperature = document.querySelector("#temperature");
+  let icon = document.querySelector("#icon");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let cityName = document.querySelector("#city-name");
+  let date = document.querySelector("#date");
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  cityName.innerHTML = response.data.name;
+  document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  getForecast(response.data.coord);
+}
+
+function citySearch(city) {
+  let apiKey = "a710bd8bd76400c9658ef649d9e81728";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  citySearch(city);
+});
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = dateFormat(currentTime);
+citySearch("Myanmar");
+showForecast();
